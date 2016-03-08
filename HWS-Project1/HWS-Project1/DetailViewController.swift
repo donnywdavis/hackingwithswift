034d35474c9b5df6eed8068869ca5108f661876d
Donny Davis <donnywdavis@icloud.com>
2016-03-08 11:59:24 -0500

    Modify the title for each navigation controller

--- HWS-Project1/HWS-Project1/DetailViewController.swift
+++ HWS-Project1/HWS-Project1/DetailViewController.swift
@@ -22,6 +22,7 @@ class DetailViewController: UIViewController {
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
+            self.title = detail
             if let imageView = self.detailImageView {
                 imageView.image = UIImage(named: detail)
             }
