34005484be487d3c37c8333a643352fc5b8e5900
Donny Davis <donnywdavis@icloud.com>
2016-03-12 13:50:30 -0500

    Use a collection view to display a grid of images and add a name to them.

---
+++ HWS-Project10/HWS-Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  HWS-Project10
+//
+//  Created by Donny on 3/12/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ HWS-Project10/HWS-Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  HWS-Project10
+//
+//  Created by Donny Davis on 3/11/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
